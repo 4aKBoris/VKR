8777226b9b1f86afec4a0ba9c85fafa7f1ec5c98
4aKBoris <lda-1028@mail.ru>
2021-06-08 01:03:14 +0300

    Добавлены настройки приложения

---
+++ app/src/main/java/mpei/vkr/ui/settings/SettingsAdapter.kt
@@ -0,0 +1,26 @@
+package mpei.vkr.ui.settings
+
+import android.content.SharedPreferences
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import mpei.vkr.ui.settings.items.cipher.SettingsCipherFragment
+import mpei.vkr.ui.settings.items.masterkey.SettingsMasterKeyFragment
+import mpei.vkr.ui.settings.items.message.digest.SettingsMessageDigestFragment
+import mpei.vkr.ui.settings.items.others.SettingsOthersFragment
+import mpei.vkr.ui.settings.items.signature.SettingsSignatureFragment
+
+class SettingsAdapter(fragment: FragmentActivity) : FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int = 5
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> SettingsCipherFragment()
+            1 -> SettingsMessageDigestFragment()
+            2 -> SettingsSignatureFragment()
+            3 -> SettingsMasterKeyFragment()
+            4 -> SettingsOthersFragment()
+            else -> SettingsCipherFragment()
+        }
+    }
+}
