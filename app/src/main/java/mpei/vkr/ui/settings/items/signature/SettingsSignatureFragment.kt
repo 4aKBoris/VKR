eb50a92c51ac1e65e8097caff1895fc1f77ae66a
4aKBoris <lda-1028@mail.ru>
2021-06-08 17:55:28 +0300

    Добавлены настройки цифровой подписи

--- app/src/main/java/mpei/vkr/ui/settings/items/signature/SettingsSignatureFragment.kt
+++ app/src/main/java/mpei/vkr/ui/settings/items/signature/SettingsSignatureFragment.kt
@@ -6,27 +6,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import mpei.vkr.R
+import mpei.vkr.databinding.SettingsMessageDigestFragmentBinding
+import mpei.vkr.databinding.SettingsSignatureFragmentBinding
+import mpei.vkr.ui.settings.items.message.digest.SettingsMessageDigestViewModel
 
 class SettingsSignatureFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = SettingsSignatureFragment()
-    }
-
+    private var _binding: SettingsSignatureFragmentBinding? = null
     private lateinit var viewModel: SettingsSignatureViewModel
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.settings_signature_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    ): View {
+        _binding = SettingsSignatureFragmentBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(SettingsSignatureViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = this
+        lifecycle.addObserver(viewModel)
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
