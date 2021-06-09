b73d0f502ea0c277ced453cfc8f5026528b4d997
4aKBoris <lda-1028@mail.ru>
2021-06-09 17:52:58 +0300

    Класс, для создания сообщений пользователю

---
+++ app/src/main/java/mpei/vkr/Others/ToastShow.kt
@@ -0,0 +1,16 @@
+package mpei.vkr.Others
+
+import android.content.Context
+import android.widget.Toast
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+internal class ToastShow {
+
+    fun show(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    suspend fun suspendShow(context: Context, message: String) =
+        withContext(Dispatchers.Main) { show(context, message) }
+}
