f99d56197a645563d015a8e4f4fcdc30021fa881
4aKBoris <lda-1028@mail.ru>
2021-06-09 17:51:40 +0300

    Добавлена возможность импорта и экспорта сетификатов

--- app/src/main/java/mpei/vkr/ui/choice/certificate/CertificateFragment.kt
+++ app/src/main/java/mpei/vkr/ui/choice/certificate/CertificateFragment.kt
@@ -1,32 +1,56 @@
+@file:Suppress("DEPRECATION")
+
 package mpei.vkr.ui.choice.certificate
 
-import androidx.lifecycle.ViewModelProvider
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import mpei.vkr.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import mpei.vkr.databinding.CertificateFragmentBinding
 
 class CertificateFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = CertificateFragment()
-    }
-
+    private var _binding: CertificateFragmentBinding? = null
     private lateinit var viewModel: CertificateViewModel
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.certificate_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    ): View {
+        _binding = CertificateFragmentBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(CertificateViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = this
+        binding.imageButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "file/*"
+            startActivityForResult(intent, PICK_FILE_RESULT_CODE)
+        }
+        binding.buttonImport.setOnClickListener { viewModel.import() }
+        binding.buttonExport.setOnClickListener { viewModel.export(binding.root.context) }
+        binding.floatingActionButton.setOnClickListener { viewModel.createCertificate(this) }
+        return binding.root
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            PICK_FILE_RESULT_CODE -> if (resultCode == Activity.RESULT_OK) viewModel.pathCertificate.value =
+                data!!.data!!.path!!.removePrefix("/root")
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        private const val PICK_FILE_RESULT_CODE = 1
+    }
 }
--- app/src/main/java/mpei/vkr/ui/choice/certificate/CertificateViewModel.kt
+++ app/src/main/java/mpei/vkr/ui/choice/certificate/CertificateViewModel.kt
@@ -1,7 +1,111 @@
 package mpei.vkr.ui.choice.certificate
 
-import androidx.lifecycle.ViewModel
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+import android.content.DialogInterface
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import kotlinx.coroutines.*
+import mpei.vkr.Constants.*
+import mpei.vkr.Exception.MyException
+import mpei.vkr.Others.FileReadWrite
+import mpei.vkr.Others.KeySt
+import mpei.vkr.Others.Notification
+import mpei.vkr.Others.ToastShow
+import mpei.vkr.R
+import java.io.File
+import java.io.FileInputStream
+import java.security.KeyStore
+import kotlin.coroutines.CoroutineContext
 
-class CertificateViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class CertificateViewModel(application: Application) : AndroidViewModel(application),
+    CoroutineScope {
+
+    @SuppressLint("StaticFieldLeak")
+    private val context = application.applicationContext
+    private val _pathCertificate = MutableLiveData("")
+
+    val pathCertificate: MutableLiveData<String> = _pathCertificate
+    val certificateName: LiveData<String> = Transformations.map(_pathCertificate) { File(it).name }
+
+    fun import() = launch(Dispatchers.IO) {
+        try {
+            val certificate = FileReadWrite(_pathCertificate.value!!).readFile()
+            val file = FileReadWrite(pathToCertificate + File(_pathCertificate.value!!).name)
+            file.writeFile(certificate)
+        } catch (e: MyException) {
+            toast.suspendShow(context, e.message!!)
+        } catch (e: Exception) {
+            toast.suspendShow(context, "Ошибка с чтением или записью сертфииката!")
+        }
+    }
+
+    fun export(context: Context) = launch(Dispatchers.IO) {
+        try {
+            var alg: String?
+            val keyStore = KeySt(password)
+            val algorithms = keyStore.algorithms()
+            if (algorithms.isNullOrEmpty()) throw MyException("Создайте сертификаты!")
+            withContext(Dispatchers.Main) {
+                val builder = AlertDialog.Builder(context)
+                builder.setTitle("Выберите сертификат")
+                builder.setItems(algorithms) { dialog, which ->
+                    alg = algorithms[which]
+                    dialog.dismiss()
+                    if (alg == null) toast.show(context, "Сертификат с алгоритмом $alg экспортирован!")
+                    else {
+                        keyStore.saveCertificate("my_cert_$alg.cer", keyStore.getCertificate(alg!!))
+                        toast.show(context, "Сертификат с алгоритмом $alg экспортирован!")
+                        Notification(context)
+                    }
+                }
+                val dialog: AlertDialog = builder.create()
+                dialog.show()
+            }
+        } catch (e: MyException) {
+            toast.suspendShow(context, e.message!!)
+        }
+    }
+
+    fun createCertificate(fragment: CertificateFragment) =
+        launch(Dispatchers.IO) {
+            val keyStoreData = FileInputStream(PATH_KEY_STORE)
+            val keyStore = KeyStore.getInstance(KEY_STORE_ALGORITHM)
+            keyStore.load(keyStoreData, password.toCharArray())
+            keyStore.aliases().toList().forEach { Log.d(LOG_TAG, it) }
+            withContext(Dispatchers.Main) {
+                if (keyStore.aliases().toList().find { it.contains(Certificate) }
+                        .isNullOrEmpty()) findNavController(fragment).navigate(R.id.action_nav_certificate_to_createCertificateFragment)
+                else dialog(fragment)
+            }
+        }
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job + CoroutineExceptionHandler { _, e -> throw e }
+
+    companion object {
+        private val job = SupervisorJob()
+        private const val password = "12345678"
+        private val toast = ToastShow()
+
+        private fun dialog(fragment: CertificateFragment) {
+            val dialogClickListener =
+                DialogInterface.OnClickListener { dialog, which ->
+                    when (which) {
+                        DialogInterface.BUTTON_POSITIVE -> findNavController(fragment).navigate(R.id.action_nav_certificate_to_createCertificateFragment)
+                        DialogInterface.BUTTON_NEGATIVE -> dialog.cancel()
+                    }
+                }
+            val builder = android.app.AlertDialog.Builder(fragment.context)
+            builder.setMessage("Сертификаты уже есть в хранилище, создать новые?")
+                .setPositiveButton("Да", dialogClickListener)
+                .setNegativeButton("Нет", dialogClickListener).show()
+        }
+    }
 }
